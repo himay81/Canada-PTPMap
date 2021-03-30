9fbdb66e55dc8637efab0843bf5ccf2d5963a515
Jamie Clinton Baxter <jamie.baxter@tutanota.com>
2021-03-30 14:20:03 -0400

    Dropped in replacement code so far.

--- ptpmap-local.py
+++ ptpmap-local.py
@@ -1,10 +1,11 @@
-import csv
-import progressbar
+#!/usr/bin/env python3
+
+import pandas as pd
+import progressbar as pg
 import simplekml
 
-csvData = open('TAFL_LTAF.csv', 'r', encoding='utf-8')
-
-taflCsv = csv.DictReader(csvData, fieldnames=[
+# Field headers for input CSV file
+names = [
     'TXRX', 'Frequency', 'FrequencyRecordIdentifier', 'RegulatoryService', 'CommunicationType',
     'ConformityToFrequencyPlan',
     'FrequencyAllocationName', 'Channel', 'LegacySystemInternationalCoordinationNumber', 'AnalogDigital',
@@ -13,7 +14,7 @@ taflCsv = csv.DictReader(csvData, fieldnames=[
     'TotalLossesDB', 'AnalogCapacity',
     'DigitalCapacity', 'RxUnfadedSignalLevel', 'RxThresholdSignalLevelBer10e', 'AntManufacturer', 'AntModel', 'AntGain',
     'AntPattern', 'HalfpowerBeamwidth',
-    'FrontToBackRatio', 'Polarization', 'HeightAboveGroundLevel', 'AzimuthOfMainLobe', 'VerticalElelevationAngle',
+    'FrontToBackRatio', 'Polarization', 'HeightAboveGroundLevel', 'AzimuthOfMainLobe', 'VerticalElevationAngle',
     'StationLocation', 'LicenseeStationReference',
     'Callsign', 'StationType', 'ITUClassOfStation', 'StationCostCategory', 'NumberOfIdenticalStations',
     'ReferenceIdentifier', 'Provinces', 'Latitude',
@@ -22,34 +23,58 @@ taflCsv = csv.DictReader(csvData, fieldnames=[
     'AuthorizationNumber', 'Service', 'Subservice', 'LicenceType', 'AuthorizationStatus', 'InserviceDate',
     'AccountNumber', 'LicenseeName', 'LicenseeAddress',
     'OperationalStatus', 'StationClassification', 'HorizontalPower', 'VerticalPower', 'StandbyTransmitterInformation'
-])
+]
 
-logFile = open("ptpmap-log.txt", "w")
+# Fields to skip on import; can be adjusted later on as necessary
+skips = [
+    'RegulatoryService', 'CommunicationType', 'ConformityToFrequencyPlan', 'FrequencyAllocationName', 'Channel',
+    'LegacySystemInternationalCoordinationNumber', 'AnalogDigital', 'DesignationOfEmission', 'ModulationType',
+    'FiltrationInstalled', 'TxERPdBW', 'TxTransmitterPowerW', 'TotalLossesDB', 'RxUnfadedSignalLevel',
+    'RxThresholdSignalLevelBer10e', 'AntManufacturer', 'AntModel', 'AntGain', 'AntPattern', 'HalfpowerBeamwidth',
+    'FrontToBackRatio', 'Polarization', 'AzimuthOfMainLobe', 'VerticalElevationAngle', 'StationLocation',
+    'LicenseeStationReference', 'Callsign', 'StationType', 'ITUClassOfStation', 'StationCostCategory',
+    'NumberOfIdenticalStations', 'ReferenceIdentifier', 'Provinces', 'GroundElevationAboveSealevel',
+    'AntennaStructureHeightAboveGroundLevel', 'CongestionZone', 'RadiusOfOperation', 'SatelliteName', 'LicenceType',
+    'AuthorizationStatus', 'AccountNumber', 'LicenseeAddress', 'OperationalStatus', 'StationClassification',
+    'HorizontalPower', 'VerticalPower', 'StandbyTransmitterInformation'
+]
 
-print("Finding TX Licenses")
+# Load all of the CSV while skipping unneeded fields
+csvd = pd.read_csv('TAFL_LTAF.csv', names=names, usecols=[n for n in names if n not in skips])
+# Temporarily drop locations with OccupiedBandwidthKHz = 0 as they appear to be relay points
+csvd = csvd.drop(csvd[csvd['OccupiedBandwidthKHz'] == 0].index)
+# zip() latitude and longitude together to ease identifying paired links
+csvd['LatLong'] = list(zip(csvd['Latitude'], csvd['Longitude']))
+# Slice out all TX and RX records for Service=(2), Subservice=(200)
+txRecords = csvd[csvd['Service'] == 2][csvd['Subservice'].isin([200])][csvd['TXRX'] == 'TX']
+rxRecords = csvd[csvd['Service'] == 2][csvd['Subservice'].isin([200])][csvd['TXRX'] == 'RX']
+# Uncomment this block for a separate set of mP2P (Subservice=(201)) locations
+# txRec201 = csvd[csvd['Service'] == 2][csvd['Subservice'].isin([201])][csvd['TXRX'] == 'TX']
+# rxRec201 = csvd[csvd['Service'] == 2][csvd['Subservice'].isin([201])][csvd['TXRX'] == 'RX']
 
-allRxRecs = []
-txRecords = []
-txLicAuthNumSet = set()
-txFreqSet = set()
-#txLatSet = set()
-#txLongSet = set()
-ptpLinks = []
+print("Found {0} TX licenses and {1} RX licenses".format(txRecords.shape[0], rxRecords.shape[0]))
+# Get a list of all the unique AuthorizationNumbers to iterate through
+txLicAuthNumSet = set(txRecords['AuthorizationNumber'])
+
+for idx, row in txRecords.iterrows():
+    rxIdx = rxRecords[rxRecords['AuthorizationNumber'] == row['AuthorizationNumber']][
+        rxRecords['Frequency'] == row['Frequency']].index[0]
+    print("TX index {0} matches RX index {1}".format(idx, rxIdx))
 
 for row in taflCsv:
-    if row['Service'] == '2' and row['Subservice'] in ['200','201'] and row['TXRX'] == 'TX':
+    if row['Service'] == '2' and row['Subservice'] in ['200', '201'] and row['TXRX'] == 'TX':
         txRecords.append(row)
         txLicAuthNumSet.add(row['AuthorizationNumber'])
         txFreqSet.add(row['Frequency'])
-        #txLatSet.add(row['Latitude'])
-        #txLongSet.add(row['Longitude'])
+    # txLatSet.add(row['Latitude'])
+    # txLongSet.add(row['Longitude'])
     if row['TXRX'] == 'RX':
         allRxRecs.append(row)
 print("Found " + str(len(txRecords)) + " licenses. Finding matching RX licenses")
 cleanrx = []
 for rec in allRxRecs:
     if rec['AuthorizationNumber'] in txLicAuthNumSet and rec['Frequency'] in txFreqSet:
-#            and rec['Latitude'] not in txLatSet and rec['Longitude'] not in txLongSet:
+        #            and rec['Latitude'] not in txLatSet and rec['Longitude'] not in txLongSet:
         cleanrx.append(rec)
 
 for txRecord in progressbar.progressbar(txRecords):
@@ -70,27 +95,27 @@ kml = simplekml.Kml()
 
 bellStyle = simplekml.Style()
 bellStyle.linestyle.width = 2
-bellStyle.linestyle.color = 'ffff0000' # Blue
+bellStyle.linestyle.color = 'ffff0000'  # Blue
 
 rogersStyle = simplekml.Style()
 rogersStyle.linestyle.width = 2
-rogersStyle.linestyle.color = 'ff0000ff' # Red
+rogersStyle.linestyle.color = 'ff0000ff'  # Red
 
 telusStyle = simplekml.Style()
 telusStyle.linestyle.width = 2
-telusStyle.linestyle.color = 'ff3CFF14' # Green
+telusStyle.linestyle.color = 'ff3CFF14'  # Green
 
 xplornetStyle = simplekml.Style()
 xplornetStyle.linestyle.width = 2
-xplornetStyle.linestyle.color = 'FF1478A0' # Brown
+xplornetStyle.linestyle.color = 'FF1478A0'  # Brown
 
 freedomStyle = simplekml.Style()
 freedomStyle.linestyle.width = 2
-freedomStyle.linestyle.color = 'ff14B4FF' # Orange
+freedomStyle.linestyle.color = 'ff14B4FF'  # Orange
 
 otherStyle = simplekml.Style()
 otherStyle.linestyle.width = 2
-otherStyle.linestyle.color = 'ffFF78F0' # Magenta
+otherStyle.linestyle.color = 'ffFF78F0'  # Magenta
 
 
 def styleLink(licName, kmlLink):
@@ -109,6 +134,7 @@ def styleLink(licName, kmlLink):
         kmlLink.style = freedomStyle
     else:
         kmlLink.style = otherStyle
+
 
 for ptp in progressbar.progressbar(ptpLinks):
     if len(ptp['rx']) == 1:
@@ -136,7 +162,7 @@ for ptp in progressbar.progressbar(ptpLinks):
         # There's not really a good way to display this for large systems like BC Hydro or Milton Hydro
         # It just looks like a mess and makes Google Earth chug, so I'm whitelisting a few interesting systems
         if ptp['tx']['Subservice'] == "201" and ptp['tx']['LicenseeName'] in [
-            'Bell Canada','Northwestel Inc.', 'Telus Communications Inc.', 'Sasktel', 'Hydro-Québec'
+            'Bell Canada', 'Northwestel Inc.', 'Telus Communications Inc.', 'Sasktel', 'Hydro-Québec'
         ]:
             for endpoint in ptp['rx']:
                 kmlLink = kml.newlinestring(
@@ -159,7 +185,7 @@ LicenseeName: {ptp['tx']['LicenseeName']}
 AuthorizationNumber: {ptp['tx']['AuthorizationNumber']}
 --------------------------\n
             """
-            #print(errorMsg)
+            # print(errorMsg)
             logFile.write(errorMsg)
 
     elif len(ptp['rx']) == 0:
@@ -172,7 +198,7 @@ LicenseeName: {ptp['tx']['LicenseeName']}
 AuthorizationNumber: {ptp['tx']['AuthorizationNumber']}
 --------------------------\n
         """
-        #print(errorMsg)
+        # print(errorMsg)
         logFile.write(errorMsg)
 
 print("Saving KML and Log")
